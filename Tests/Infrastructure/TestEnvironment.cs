9cfd372690f9be8b359035de7260020a947c98a9
Hans Gomez <hans.gomez@nserio.com>
2018-04-12 11:45:50 -0500

    automation script

---
+++ Tests/Infrastructure/TestEnvironment.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Relativity.API;
+using Relativity.Test.Helpers;
+using System;
+
+namespace CreateObjectAfterFieldParsing.Infrastructure
+{
+    class TestEnvironment : IDisposable
+    {
+        public static TestEnvironment Instance => _instance.Value;
+        private static Lazy<TestEnvironment> _instance = new Lazy<TestEnvironment>(() => new TestEnvironment());
+
+        /// <summary>
+        /// In charge of interface interaction
+        /// </summary>
+        public IWebDriver Driver { get; }
+        /// <summary>
+        /// In charge of API-BackEnd interaction
+        /// </summary>
+        public TestHelper BackendHelper { get; }
+        /// <summary>
+        /// Wrapper for GetServicesManager
+        /// </summary>
+        public IServicesMgr ServicesManager => BackendHelper.GetServicesManager();
+
+        private TestEnvironment()
+        {
+            BackendHelper = new TestHelper();
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected internal void Dispose(bool disposing)
+        {
+            //Driver.Close();
+            //Driver.Dispose();
+        }
+
+        ~TestEnvironment()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Tests/MainTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using SeleniumUnitTest;
+
+namespace CreateObjectAfterFieldParsing
+{
+    [TestFixture]
+    public class MainTests
+    {
+        Helper helper = new Helper();
+        [SetUp]
+        public void Setup()
+        {
+            PageInitial home = new PageInitial();
+            home.GoToPage();
+            home.LoginToApplication();
+        }
+
+        [Test]
+        [Category("UC1 - CombineField")]
+        public void RunScriptCombineFieldTest()
+        {
+            // -- arrange: dedicated to collect input data
+            helper.ProvisionEnvironment();
+            helper.ClickToWorkSpace();
+            helper.ClickTabAdmin();
+
+
+            //--- act: run process
+            helper.RunScript();
+            var retuned = helper.SelectTypeSearchCombineFields();
+
+            //--- assert: validate results
+            Assert.IsTrue(retuned);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            helper.CleanEnvironment();
+        }
+    }
+}
